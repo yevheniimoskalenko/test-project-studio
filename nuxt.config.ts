92e0485ba080a10b83283e73a3073280f58e733e
yevhenii <testjokerqwerty@gmail.com>
2026-01-11 12:16:20 +0200

    fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const {
+  STUDIO_GITHUB_CLIENT_ID,
+  STUDIO_GITHUB_CLIENT_SECRET,
+} = process.env
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/eslint', 'nuxt-studio'],
+    runtimeConfig: {
+    public: {
+      STUDIO_GITHUB_CLIENT_ID,
+      STUDIO_GITHUB_CLIENT_SECRET
+    }
+  },
   studio: {
+    dev: true,
     repository: {
       provider: 'github', // 'github' or 'gitlab'
       owner: 'yevheniimoskalenko',
