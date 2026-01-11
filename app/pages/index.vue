249fd8e100b709dac1cce82da6b865abd94ba93d
yevhenii <testjokerqwerty@gmail.com>
2026-01-11 12:21:29 +0200

    added ContentRenderer

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+</script>
 <template>
-    <div>
-        home
-    </div>
+<ContentRenderer v-if="page" :value="page" />
 </template>
